2647f1c2bc4efa291fbb63cd0400669956641875
kuwphmi <jabbarfahmi23@gmail.com>
2025-10-06 13:46:50 +0700

    Baru Fress BGT

--- Project Absensi/My Project/Application.Designer.vb
+++ Project Absensi/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Project_Absensi.Login
+            Me.MainForm = Global.Project_Absensi.Welcome
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
